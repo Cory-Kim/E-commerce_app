ae554318621f3428ce4a261e9696426660d70df4
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-05-08 19:22:00 -0700

    checking if env is uploading or not

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ const {default: mongoose} = require("mongoose")
 const dbConnect = () =>
 {
   try {
-    const connect = mongoose.connect('mongodb://localhost:27017/ecommerce');
+    const connect = mongoose.connect(process.env.MONGODB_URL);
     console.log('Database Connected Successfully')
 
   } catch (error) {
