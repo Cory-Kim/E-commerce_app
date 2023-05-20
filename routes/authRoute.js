1b97b23671eab61cb15bef8012985d8639f52cff
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-05-20 03:14:41 -0700

    added create, get, update order function.

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -19,7 +19,10 @@ const {
   userCart,
   getUserCart,
   emptyCart,
-  applyCoupon
+  applyCoupon,
+  createOrder,
+  getOrder,
+  updateOrderStatus
 
 } = require('../controller/userCtrl');
 const {authMiddleware, isAdmin} = require('../middlewares/authMiddleware');
@@ -28,14 +31,18 @@ const router = express.Router();
 router.post('/register', createUser);
 router.post('/forgot-password-token', forgotPasswordToken);
 router.put('/reset-password/:token', resetPassword);
+router.put('/order/update-order/:id', authMiddleware, isAdmin, updateOrderStatus);
 router.put('/password', authMiddleware, updatePassword);
 
 router.post('/login', loginUserCtrl);
 router.post('/admin-login', loginAdmin);
 router.post('/cart', authMiddleware, userCart);
 router.post('/cart/apply-coupon', authMiddleware, applyCoupon);
+router.post('/cart/cash-order', authMiddleware, createOrder);
+
 router.get('/all-users', getAllUser);
 
+router.get('/get-order', authMiddleware, getOrder);
 router.get('/refresh', handleRefreshToken);
 router.get('/logout', logout);
 router.get('/wishlist-get', authMiddleware, getWishlist);
